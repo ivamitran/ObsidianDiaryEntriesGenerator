348c4196a11a69cca21c9955f99e0a4210ae10f1
Ivan Tran <ivamitran16432@gmail.com>
2023-12-30 17:34:07 -0800

    This program is very similar to my ObsidianDailyToDoListsGenerator. It works in the same manner, but the markdown files are slightly different in terms of content

---
+++ src/DiaryEntryNote.java
@@ -0,0 +1,79 @@
+import java.time.LocalDate;
+import java.util.Arrays;
+
+public class DiaryEntryNote extends ObsidianNote {
+
+    // first X: the date (e.g., 27)
+    // second X: abbreviation of the month (e.g., dec for December)
+    // third X: the year (e.g., 2023)
+    static String generalToDoListNoteFileName = "XXX";
+
+    static String[] generalNoteContent = {
+            "", // this will be left empty
+            "---",
+            "Back to Root Note: [[X]]", // X: file name of dated to do list root note
+            "Previous Diary Entry: [[X]]", // X: file name of previous dated to do list note
+            "Next Diary Entry: [[X]]" // X: file name of next dated to do list note
+    };
+
+    String dayOfMonth;
+    String month;
+    String year;
+
+    public DiaryEntryNote(String fileName, String[] noteContent, String dayOfMonth, String month, String year){
+        super(fileName, noteContent);
+        this.dayOfMonth = dayOfMonth;
+        this.month = month;
+        this.year = year;
+    }
+
+    /**
+     *
+     * @param currentDate This is the date of which the specific note content will be for.
+     *                    We need this so that we can determine the previous date and the next date which are needed to generate the specific note content
+     * @return the specific note content (file content) of current to do list
+     */
+    public static String[] returnSpecificNoteContent(LocalDate currentDate)
+    {
+        String[] specificNoteContent = Arrays.copyOf(generalNoteContent, generalNoteContent.length);
+
+        LocalDate prevDate = currentDate.minusDays(1);
+        LocalDate nextDate = currentDate.plusDays(1);
+
+        String[] prevDateRelevantStrings = DateFunctions.returnLocalDateRelevantStrings(prevDate);
+        String prevDateDayOfMonth = prevDateRelevantStrings[0];
+        String prevDateAbbrevMonth = prevDateRelevantStrings[1];
+        String prevDateYear = prevDateRelevantStrings[2];
+        String prevDateFileName = prevDateDayOfMonth + prevDateAbbrevMonth + prevDateYear;
+
+        String[] nextDateRelevantStrings = DateFunctions.returnLocalDateRelevantStrings(nextDate);
+        String nextDateDayOfMonth = nextDateRelevantStrings[0];
+        String nextDateAbbrevMonth = nextDateRelevantStrings[1];
+        String nextDateYear = nextDateRelevantStrings[2];
+        String nextDateFileName = nextDateDayOfMonth + nextDateAbbrevMonth + nextDateYear;
+
+        int count = 0; // used to determine which X currently replacing in order to replace with the correct value
+        for (int i = 0; i < specificNoteContent.length; i++) {
+            if (specificNoteContent[i].contains("X")) {
+                count++;
+                if (count == 1) // this X is for the file name of the root note
+                {
+                    specificNoteContent[i] = specificNoteContent[i].replaceFirst("X", DiaryEntryNotesFramework.diaryEntryNotesRootNote);
+                }
+                else if (count == 2) // this X is for the file name of the previous note
+                {
+                    specificNoteContent[i] = specificNoteContent[i].replaceFirst("X", prevDateFileName);
+                }
+                else if (count == 3) // this X is for the file name of the next note
+                {
+                    specificNoteContent[i] = specificNoteContent[i].replaceFirst("X", nextDateFileName);
+                    break;
+                }
+            }
+        }
+
+        return specificNoteContent;
+    }
+
+
+}
---
+++ src/DiaryEntryNotesFramework.java
@@ -0,0 +1,66 @@
+import java.time.LocalDate;
+import java.util.List;
+
+public class DiaryEntryNotesFramework {
+    static String diaryEntryNotesRootNote = "diaryRootNote";
+
+    // this list will hold the LocalDate objects that correspond to each DiaryEntryNote object
+    // is List and not LocalDate[] because the method used to generate the sequence of LocalDates returns a list
+    List<LocalDate> localDates;
+    DiaryEntryNote[] diaryEntryNotes; // this array will hold the individual DatedToDoListNote objects that will be part of the framework
+
+    public DiaryEntryNotesFramework(LocalDate startDate, LocalDate endDate)
+    {
+        localDates = DateFunctions.generateSequentialDates(startDate, endDate);
+        diaryEntryNotes = new DiaryEntryNote[localDates.size()];
+
+        // instantiate the individual DiaryEntryNote objects and store them into the array
+        for(int i = 0; i < diaryEntryNotes.length; i++)
+        {
+            // generate the file name of the markdown file
+            String[] currentDateRelevantStrings = DateFunctions.returnLocalDateRelevantStrings(localDates.get(i));
+            String dayOfMonth = currentDateRelevantStrings[0];
+            String abbreviatedMonth = currentDateRelevantStrings[1];
+            String year = currentDateRelevantStrings[2];
+            String fileName = dayOfMonth + abbreviatedMonth + year;
+
+            // generate the note content
+            String[] specificNoteContent = DiaryEntryNote.returnSpecificNoteContent(localDates.get(i));
+
+            diaryEntryNotes[i] = new DiaryEntryNote(fileName, specificNoteContent, dayOfMonth, abbreviatedMonth, year);
+        }
+    }
+
+    void generateFiles()
+    {
+        for(DiaryEntryNote diaryEntryNote : diaryEntryNotes)
+        {
+            diaryEntryNote.generateNoteFile();
+        }
+    }
+
+    public static void main(String[] args) {
+        LocalDate startDate = null;
+        LocalDate endDate = null;
+
+        boolean validStartAndEndDatesEntered = false; // assume false initially
+
+        // before moving forward, have to make sure that the end date comes after the start date
+        while(!validStartAndEndDatesEntered)
+        {
+            System.out.println("***Enter information for the start date***");
+            startDate = DateFunctions.promptForDate();
+            System.out.println("***Enter information for the end date***");
+            endDate = DateFunctions.promptForDate();
+
+            if(startDate.isBefore(endDate))
+            {
+                validStartAndEndDatesEntered = true;
+            }
+        }
+
+        DiaryEntryNotesFramework diaryEntryNotesFramework = new DiaryEntryNotesFramework(startDate, endDate);
+        diaryEntryNotesFramework.generateFiles();
+        return;
+    }
+}
